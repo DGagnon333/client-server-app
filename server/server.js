6240db5f28772dd9a7fa21cbd4a5f72db723a646
derickGagnon <derick.gag46@hotmail.com>
2023-10-08 22:23:52 -0700

    Adds routes, env and rate-limiting

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
 
-app.get('/api', (req, res) => {
-    res.json({"exercices": ["pushups", "setups", "dbcurls"]})
-})
+app.use(cors());
 
-app.listen(5000, () => console.log('Server running on port 5000'))
+const PORT = process.env.PORT;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Max requests per window
+});
+
+app.use(limiter);
+
+//for developpment
+app.use(cors({ origin: "*" }));
+
+//for production
+//app.use(cors({ origin: <client-url> }));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
